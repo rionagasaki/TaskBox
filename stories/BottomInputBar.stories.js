78f11d1828da135eb23128138ef216799433ed8d
nagasakirio <naga_ri@icloud.com>
2022-08-23 17:02:14 +0900

    'button-text-changes'

--- stories/BottomInputBar.stories.js
+++ stories/BottomInputBar.stories.js
@@ -1,4 +1,5 @@
 import BottomInputBar from './BottomInputBar.vue'
+import { action } from '@storybook/addon-actions';
 import vuetify from '../plugins/vuetify'
 export default {
     title: 'Chat/Input',
@@ -12,7 +13,11 @@ const Template = (args, { argTypes }) => ({
     components: { BottomInputBar },
     props: Object.keys(argTypes),
     vuetify,
-    template: '<bottom-input-bar v-bind="$props" @sendMessage="sendMessage" />',
+    template: '<bottom-input-bar v-bind="$props" @sendMessage="sendMessage" @changeMessage="changeMessage" />',
+    methods:{
+        sendMessage: action('send!'),
+        changeMessage: action('send!')
+    }
 })
 
 export const InputBar = Template.bind({});
